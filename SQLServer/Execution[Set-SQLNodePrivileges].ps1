b171176411aea9fbafc7ec3e76478b86c95c72bb
Paul Maxfield <paul.dmax@outlook.com>
2016-10-17 23:01:44 +0100

    Set-SQLNodePrivileges
    Sets Perform Volume Maintenance Tasks and Lock pages in memory for Service Account.
    
    Example
    $ServiceAccount = "SQL-ServiceAccount"
    
    Set-SQLNodePrivileges -ServiceAccount $ServiceAccount

---
+++ SQLServer/Execution[Set-SQLNodePrivileges].ps1
@@ -0,0 +1,13 @@
+﻿
+    $VMName     = $env:COMPUTERNAME
+
+    [string]$SA = 'SQLSVCACCOUNT'
+    $CnfgFile   = "\\library\AllSQLUpdates\ConfigurationFiles\$VMName-ConfigurationFile.ini"
+    $acc        = Get-Content $CnfgFile -Encoding ascii -ErrorAction SilentlyContinue|Select-String $SA
+
+    if ($acc)
+    {
+        [string]$ServiceAccount = ($acc -split ('=')).Replace("`"","")[-1]
+        if($ServiceAccount)
+            {Set-SQLNodePrivileges -ServiceAccount $ServiceAccount}
+    }
