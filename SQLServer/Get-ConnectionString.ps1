df2a4d17a6f8fc2fb79ba9da4adaec67fc9bf6b2
Paul Maxfield <paul.dmax@outlook.com>
2016-10-18 19:19:54 +0100

    Test and Record Service Accounts
    Set of functions to test service account and store account name and password into a database.
    Functions:
    Test-ServiceAccount.ps1
    Set-SAAccount.ps1
    Get-ConnectionString.ps1
    Get-uspSetSAAccount.ps1
    
    Example:
    $SQLServer  = "SQL-LD02"
    $SAAccount  = "SQLHORIZONS\SA-SQLLD02-SQLServer"
    Get-Credential -Credential $SAAccount|
        Test-ServiceAccount -Verbose|
            Set-SAAccount -SQLServer $SQLServer -DatabaseServer "S1" -Database "SQLInfo" -Verbose

---
+++ SQLServer/Get-ConnectionString.ps1
@@ -0,0 +1,14 @@
+﻿Function Get-ConnectionString
+{
+    [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
+    param
+    (
+        [parameter(Mandatory = $true)]
+        [object]$Server,
+        [parameter(Mandatory = $true)]
+        [object]$Database,
+        [object]$IntegratedSecurity = $true
+    )
+
+    Return "Server=$($Server);Database=$($Database);Integrated Security=$($IntegratedSecurity);"
+}
---
+++ SQLServer/Get-uspSetSAAccount.ps1
@@ -0,0 +1,34 @@
+﻿Function Get-uspSetSAAccount
+{
+    [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
+    param
+    (
+        [parameter(Mandatory = $true)]
+        [object]$SqlConnection,
+        [parameter(Mandatory = $true)]
+        [string]$SQLServer,
+        [parameter(Mandatory = $true)]
+        [pscredential]$SACredential 
+    )
+
+    [string]$StoredProcedure  = "[SQLOPs].[Set-SAAccount]"
+    [string]$pramNS           = "Data.SqlClient.SqlParameter"
+
+    Write-Verbose "Creating New Object for SqlCommand: $($StoredProcedure)..."
+    $SqlCommand = New-Object -TypeName System.Data.SqlClient.SqlCommand($StoredProcedure,$SqlConnection)
+
+    $SqlCommand.CommandType = [System.Data.CommandType]::StoredProcedure
+
+    Write-Verbose "Creating Parameters for SqlCommand..."
+    $SqlCommand.Parameters.Add((New-Object $pramNS('@SQLSrv',       [Data.SQLDBType]::VarChar, 20)))|Out-Null
+    $SqlCommand.Parameters.Add((New-Object $pramNS('@SA_Account',   [Data.SQLDBType]::VarChar, 20)))|Out-Null
+    $SqlCommand.Parameters.Add((New-Object $pramNS('@SA_AccountPWD',[Data.SQLDBType]::VarChar, 50)))|Out-Null
+
+    Write-Verbose "Setting Parameters for SqlCommand..."
+    $SqlCommand.Parameters[0].Value = $SQLServer
+    $SqlCommand.Parameters[1].Value = ($($SACredential.username).split('\')[-1]).Replace("`"","")
+    $SqlCommand.Parameters[2].Value = $SACredential.GetNetworkCredential().password
+
+    Write-Verbose "Passing back SqlCommand object..."
+    Return $SqlCommand
+}
