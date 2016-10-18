5bcfafdbbc0e15621100fcc17634bc8ad84b5096
Paul Maxfield <paul.dmax@outlook.com>
2016-10-18 09:01:30 +0100

    Add files via upload
    Processes a list of standard directories to create.
    
    Example:
    
    $SQLdirectories = "T:\Data_X\diag_01","T:\Data_X\FTData","T:\Data_X\oracle","T:\Data_X\Packages","T:\Data_X\PoSh","T:\Data_X\tivoli"
    New-DirectorySet -directories $SQLdirectories -Verbose

---
+++ SQLServer/New-DirectorySet.ps1
@@ -0,0 +1,15 @@
+﻿Function New-DirectorySet
+{
+    [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
+    param
+    (
+        [parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [object]$directories
+    )
+
+    foreach ($directory in $directories)
+    {
+        $null = New-Item -Path $directory -ItemType directory -Force
+        Write-Verbose "Directory $($directory) exists $(Test-Path -Path $directory)."
+    }
+}
