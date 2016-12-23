e23594a659b5a2da1e9994b3c84da2e15943da08
Paul Maxfield <paul.dmax@outlook.com>
2016-12-23 13:53:15 +0000

    Update Grant-SPNModify.ps1
    Update to allow Credential to make change

--- Server/Modules/Grant-SPNModify.ps1
+++ Server/Modules/Grant-SPNModify.ps1
@@ -4,22 +4,42 @@ Function Grant-SPNModify
     param
     (
         [parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [ADSI]$TargetObject,
+        [object]$Account,
         [parameter(Mandatory = $true)]
         [pscredential]$Credential
     )
-    
-    [Security.Principal.IdentityReference]$Identity = [security.principal.ntaccount]"NT AUTHORITY\SELF"
-    
-    $adRight = [DirectoryServices.ActiveDirectoryRights]"readproperty,writeproperty"
+
+    Write-Verbose "[Starting] $($MyInvocation.Mycommand)" 
+
+    $do = {
+    Param(
+    $ADObject
+    )
     
     $rootDSE = [ADSI]"LDAP://RootDSE"
     $schemaDN = $rootDSE.psbase.properties["schemaNamingContext"][0]
     $spnEntry = [ADSI]"LDAP://CN=Service-Principal-Name,$schemaDN"
     $spnSecGuid = New-Object -TypeName System.Guid @( ,$spnEntry.psbase.Properties["schemaIDGUID"][0])
+
+    [Security.Principal.IdentityReference]$Identity = [security.principal.ntaccount]"NT AUTHORITY\SELF"
+    
+    $adRight = [DirectoryServices.ActiveDirectoryRights]"readproperty,writeproperty"
     
     $spnAce = New-Object -TypeName DirectoryServices.ActiveDirectoryAccessRule($identity, $adRight, "Allow", $spnSecGuid, "None")
-    
+
+    $TargetObject = [ADSI]"LDAP://$($ADObject.DistinguishedName)"
     $TargetObject.psbase.ObjectSecurity.AddAccessRule($spnAce)
     $TargetObject.psbase.CommitChanges()
+    }
+
+    $InvokeCmdparam = @{
+        ScriptBlock = $do
+        ArgumentList = $Account
+        Credential = $Credential
+        ComputerName = $env:COMPUTERNAME
+    }
+
+    Invoke-Command @InvokeCmdparam #| Select-Object * -ExcludeProperty runspaceID
+
+    Write-Verbose "[Ending]  $($MyInvocation.Mycommand)" 
 }
