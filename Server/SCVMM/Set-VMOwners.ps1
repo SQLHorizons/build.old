077134a2d27f41db39579d2b8425d8469b279a53
Paul Maxfield <paul.dmax@outlook.com>
2017-02-24 12:05:32 +0000

    Create Set-VMOwners.ps1

---
+++ Server/SCVMM/Set-VMOwners.ps1
@@ -0,0 +1,20 @@
+Function Set-VMOwners
+{
+    [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
+    param
+    (
+        [parameter(Mandatory = $true)]
+        [string]$VMName,
+        [string]$Cluster = "OCCloud1"
+    )
+
+    $resource = Get-ClusterGroup -Cluster $Cluster|
+        Where-Object {$_.Name -like "*$VMName*"}
+
+    switch -Wildcard (($resource.ownernode -split '')[6])
+    {
+        "1" {Set-ClusterOwnerNode -InputObject $resource -Owners $((Get-SCVMHostCluster -Name $Cluster).Nodes.Computername|Where-Object {($_ -split '')[6] -eq 1});break}
+        "2" {Set-ClusterOwnerNode -InputObject $resource -Owners $((Get-SCVMHostCluster -Name $Cluster).Nodes.Computername|Where-Object {($_ -split '')[6] -eq 2});break}
+        default {Throw "Failed to find owners"}
+    }
+}
