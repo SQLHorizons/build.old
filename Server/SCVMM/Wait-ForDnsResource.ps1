c18b74b818b482b66cfb11fc1f03d1bd018f4edb
Paul Maxfield <paul.dmax@outlook.com>
2017-03-02 16:06:52 +0000

    Create Wait-ForDnsResource.ps1

---
+++ Server/SCVMM/Wait-ForDnsResource.ps1
@@ -0,0 +1,26 @@
+Function Wait-ForDnsResource 
+{
+    [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
+    param
+    (
+        [parameter(Mandatory = $true)]
+        [string]$VMName,
+        [parameter(Mandatory = $true)]
+        [string]$DNServer
+    )
+    $i = 0
+
+    $DnsServerResourceRecord = @{
+        ZoneName = $env:USERDNSDOMAIN
+        Name = $VMName
+        ComputerName = $DNServer
+        ErrorAction = "SilentlyContinue"
+        }
+
+    while (-not(Get-DnsServerResourceRecord @DnsServerResourceRecord))
+    {
+        Write-Host $i
+        Start-Sleep -Seconds 5
+        $i++
+    }
+}
