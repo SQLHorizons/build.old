325b640f45efa349fbfcfe9f3e3a18fa9f6845db
Paul Maxfield <paul.dmax@outlook.com>
2017-03-06 21:56:13 +0000

    Update and rename Set-DisksOnline.ps1 to Initialize-Disks.ps1

--- Clustering/Initialize-Disks.ps1
+++ Clustering/Initialize-Disks.ps1
@@ -1,10 +1,11 @@
-Function Set-DisksOnline
+Function Initialize-Disks
 {
     foreach ($disk in Get-Disk|Where-Object PartitionStyle -eq 'RAW'|Sort-Object Number)
     {
         If ($disk.OperationalStatus -eq 'Offline')
         {
             Set-Disk -Number $disk.Number -IsOffline $False
+            Initialize-Disk -Number $disk.Number -PartitionStyle GPT
         }
     }
 }
