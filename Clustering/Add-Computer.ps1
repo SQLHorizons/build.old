708eb5d00966b6407e946424a97ebbd479ea6812
Paul Maxfield <paul.dmax@outlook.com>
2017-03-08 14:30:00 +0000

    Update Add-Computer.ps1

--- Clustering/Add-Computer.ps1
+++ Clustering/Add-Computer.ps1
@@ -1,5 +1,10 @@
 $OUPath = "OU=SQL,OU=MER1 Hyper-V,OU=Production Virtual Servers,OU=OCC,OU=Member Servers,DC=norfolk,DC=police,DC=uk"
 
+If ((Get-Content "$($env:windir)\system32\Drivers\etc\hosts") -notcontains "10.34.81.2 is-oc02.norfolk.police.uk")   
+{
+    Add-Content -Encoding UTF8  "$($env:windir)\system32\Drivers\etc\hosts" "10.34.81.2 is-oc02.norfolk.police.uk"
+}
+
 $AddComputer = @{
     DomainName = "NORFOLK"
     Server = "IS-OC02"
