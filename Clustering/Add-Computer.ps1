1fd0d1cd354e8684db59b4d3ecfccc4488e47e8c
Paul Maxfield <paul.dmax@outlook.com>
2017-03-08 14:14:51 +0000

    Create Add-Computer.ps1

---
+++ Clustering/Add-Computer.ps1
@@ -0,0 +1,11 @@
+$OUPath = "OU=SQL,OU=MER1 Hyper-V,OU=Production Virtual Servers,OU=OCC,OU=Member Servers,DC=norfolk,DC=police,DC=uk"
+
+$AddComputer = @{
+    DomainName = "NORFOLK"
+    Server = "NORFOLK\IS-OC02"
+    OUPath = $OUPath
+    Credential = $(Get-Credential -UserName "NORFOLK\Administrator" -Message "Enter Password")
+    Restart = $true
+    }
+
+Add-Computer @AddComputer
