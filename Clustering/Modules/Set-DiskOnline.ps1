98eb19981476b6abcee05d3d366c70f39d553f3f
Paul Maxfield <paul.dmax@outlook.com>
2017-04-10 10:56:21 +0100

    Update Set-DiskOnline.ps1

--- Clustering/Modules/Set-DiskOnline.ps1
+++ Clustering/Modules/Set-DiskOnline.ps1
@@ -4,16 +4,26 @@ Function Set-DiskOnline
     (
         [object]$Disk
     )
- 
+
     if ($Disk.IsOffline -eq $true)
     {
         Write-Verbose "Setting      disk number '$($Disk.Number)' Online"
         $Disk | Set-Disk -IsOffline $false
+
+        if ((Get-Disk -Number $Disk.Number).IsOffline -eq $true)
+        {
+            Throw "Failed to set disk number '$($Disk.Number)' Online"
+        }
     }
         
     if ($Disk.IsReadOnly -eq $true)
     {
         Write-Verbose "Setting      disk number '$($Disk.Number)' to not ReadOnly"
         $Disk | Set-Disk -IsReadOnly $false
+
+        if ((Get-Disk -Number $Disk.Number).IsReadOnly -eq $true)
+        {
+            Throw "Failed to set disk number '$($Disk.Number)' to not ReadOnly"
+        }
     }
 }
