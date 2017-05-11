2777b60b9a837a7a564f7f49da3e42800d4bcc20
Paul Maxfield <paul.dmax@outlook.com>
2017-05-11 15:51:43 +0100

    Update flipflopdnsRecord.ps1

--- Server/flipflopdnsRecord.ps1
+++ Server/flipflopdnsRecord.ps1
@@ -1,12 +1,32 @@
-[System.Net.IPAddress]$dboc56 = "10.34.82.49"
-[System.Net.IPAddress]$dbde56 = "10.129.81.35"
-
-$oldhostRecord = Get-DnsServerResourceRecord -ComputerName IS-OC02 -ZoneName norfolk.police.uk -Name mitel-db
-$newhostRecord = $oldhostRecord.PSObject.Copy()
-switch ($oldhostRecord.RecordData.IPv4Address)
+function Switch-DNSResourceRecord
 {
-    $dboc56 {$newhostRecord.RecordData.IPv4Address = $dbde56}
-    $dbde56 {$newhostRecord.RecordData.IPv4Address = $dboc56}
+    [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
+    param
+    (
+        [parameter(Mandatory = $false)]
+        [System.Net.IPAddress]$ipAddr1 = "10.34.82.49",
+        [parameter(Mandatory = $false)]
+        [System.Net.IPAddress]$ipAddr2 = "10.129.81.35",
+        [parameter(Mandatory = $false)]
+        [string]$dnsServer             = "IS-OC02"
+    )
+
+    $CommonParameters = @{
+        ComputerName = $dnsServer
+        ZoneName     = $($env:USERDNSDOMAIN.ToLower())
+        Verbose      = $true
+        }
+
+    $oldhostRecord = Get-DnsServerResourceRecord @CommonParameters -Name mitel-db
+    $newhostRecord = $oldhostRecord.PSObject.Copy()
+
+    switch ($oldhostRecord.RecordData.IPv4Address)
+    {
+        $ipAddr1 {$newhostRecord.RecordData.IPv4Address = $ipAddr2}
+        $ipAddr2 {$newhostRecord.RecordData.IPv4Address = $ipAddr1}
+    }
+    Set-DnsServerResourceRecord @CommonParameters -NewInputObject $newhostRecord -OldInputObject $oldhostRecord
+    Return $newhostRecord.RecordData.IPv4Address
 }
-Set-DnsServerResourceRecord -NewInputObject $newhostRecord -OldInputObject $oldhostRecord -ZoneName norfolk.police.uk -ComputerName IS-OC02
-$newhostRecord.RecordData.IPv4Address
+
+Switch-DNSResourceRecord -Verbose
